28304da911a9f55dce6769fb74a75ac703b58170
geniushyeon <jhlee0912@icloud.com>
2021-04-08 16:50:03 +0900

    [ADD] 컴포넌트 스캔

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -27,6 +27,7 @@ public class AppConfig {
         System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
+
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
