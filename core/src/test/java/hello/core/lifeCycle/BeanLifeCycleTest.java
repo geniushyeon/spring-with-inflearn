4d65a3944891ce928a2be6c048b9b571bab86cfc
geniushyeon <jhlee0912@icloud.com>
2021-04-18 18:24:21 +0900

    📝✅ 빈 생명주기 콜백

---
+++ core/src/test/java/hello/core/lifeCycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.JavaBean;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        // ApplicationContext에는 없는 메서드
+        ac.close(); // 닫아주기
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+
+            return networkClient;
+        }
+    }
+}
